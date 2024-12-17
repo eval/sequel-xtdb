051f2c78ab6aa12d3e49d3231e4171d36f93eeea
Gert Goet <gert@thinkcreate.dk>
2024-12-17 15:52:40 +0100

    Apply timezone settings

--- lib/sequel/adapters/xtdb.rb
+++ lib/sequel/adapters/xtdb.rb
@@ -13,6 +13,15 @@ module Sequel
       def adapter_initialize
         # XTDB can't handle this SET-command
         @opts[:force_standard_strings] = false
+
+        Sequel.database_timezone = :utc
+        Sequel.application_timezone = :local
+
+        if (app_tz = @opts[:application_timezone])
+          Sequel.extension(:named_timezones)
+          Sequel.application_timezone = app_tz
+        end
+
         super
       end
 
@@ -23,6 +32,12 @@ module Sequel
 
     class Dataset < Sequel::Postgres::Dataset
       include ::Sequel::XTDB::DatasetMethods
+
+      private
+
+      def default_timestamp_format
+        "'%Y-%m-%d %H:%M:%S'"
+      end
     end
   end
 end
